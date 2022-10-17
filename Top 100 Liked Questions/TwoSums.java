67c00e89504fb4d76160b627f1aaf4b579955d1c
mpfdev <matheus.filgueiras17@gmail.com>
2022-10-17 08:33:47 -0300

    add: twosums.java leetcode

---
+++ Top 100 Liked Questions/TwoSums.java
@@ -0,0 +1,21 @@
+class TwoSums {
+    public int[] twoSum(int[] nums, int target) {
+        int z = 0;
+        int y = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if (i == j)
+                    break;
+                
+                if(nums[i] + nums[j] == target) {
+                z = i;
+                y = j;
+                break;
+            }
+            }
+        }
+        
+        int[] arr = {z,y};
+        return arr;
+    }
+}
